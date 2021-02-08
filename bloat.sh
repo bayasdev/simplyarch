67f750ebd8da5b594d208984206649e179b2e367
Lenus Walker <walker.lenus@gmail.com>
2021-02-08 12:52:14 -0400

    testing paru install.

--- bloat.sh
+++ bloat.sh
@@ -76,6 +76,6 @@ then
     then
         arch-chroot /mnt /bin/bash -c "flatpak install flathub -y --noninteractive --app org.gnome.Boxes org.gnome.Calculator org.gnome.Calendar org.gnome.Characters org.gnome.clocks org.gnome.eog org.gnome.Epiphany org.gnome.Extensions org.gnome.Evince org.gnome.FileRoller org.gnome.font-viewer org.gnome.Geary org.gnome.gedit org.gnome.Photos org.gnome.Totem org.gnome.Weather org.libreoffice.LibreOffice"
     fi
-    arch-chroot -u $user /mnt /bin/bash -c "paru -Sy timeshift-bin --removemake --cleanafter --noconfirm"
+    arch-chroot -u $user /mnt /bin/bash -c "HOME=/home/$user; paru -Sy timeshift-bin --removemake --cleanafter --noconfirm"
 fi
 exit 0
