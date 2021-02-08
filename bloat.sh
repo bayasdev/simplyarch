981a7bece93981abeb157f2843a9df3218eac04a
Lenus Walker <walker.lenus@gmail.com>
2021-02-08 12:33:04 -0400

    cleanup bloat.sh. still trying to get paru install.

--- bloat.sh
+++ bloat.sh
@@ -68,13 +68,14 @@ echo
 echo "Install my customs? (Y/N)"
 echo "These customs includes:"
 echo "- flatpak applications (libre office and other general flatpaks)"
-echo "- alacritty (terminal emulator)"
 echo "- timeshift (for snapshots)"
 read -p "My Customs: " custom
 if [[ $custom == "y" || $custom == "Y" || $custom == "yes" || $custom == "Yes" ]]
 then
-    pacstrap /mnt alacritty
-    arch-chroot /mnt /bin/bash -c "flatpak install flathub -y --noninteractive --app org.gnome.Boxes org.gnome.Calculator org.gnome.Calendar org.gnome.Characters org.gnome.clocks org.gnome.eog org.gnome.Epiphany org.gnome.Extensions org.gnome.Evince org.gnome.FileRoller org.gnome.font-viewer org.gnome.Geary org.gnome.gedit org.gnome.Photos org.gnome.Totem org.gnome.Weather org.libreoffice.LibreOffice"
-    arch-chroot -u $user /mnt /bin/bash -c "paru -Sy timeshift --removemake --cleanafter"
+    if [[ $flatpak == "y" || $flatpak == "Y" || $flatpak == "yes" || $flatpak == "Yes" ]]
+    then
+        arch-chroot /mnt /bin/bash -c "flatpak install flathub -y --noninteractive --app org.gnome.Boxes org.gnome.Calculator org.gnome.Calendar org.gnome.Characters org.gnome.clocks org.gnome.eog org.gnome.Epiphany org.gnome.Extensions org.gnome.Evince org.gnome.FileRoller org.gnome.font-viewer org.gnome.Geary org.gnome.gedit org.gnome.Photos org.gnome.Totem org.gnome.Weather org.libreoffice.LibreOffice"
+    fi
+    arch-chroot -u $user /mnt /bin/bash -c "paru -Sy timeshift-bin --removemake --cleanafter --noconfirm"
 fi
 exit 0
