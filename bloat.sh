6d1222074e298ed58f054cb9fbd14a630d1c9aa3
Victor Bayas <victorsbayas@gmail.com>
2021-02-09 01:34:06 -0500

    Add broadcom and vaapi

--- bloat.sh
+++ bloat.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 clear
-echo ">>> Bloat installer <<<"
+echo ">>> SimplyArch bloat installer <<<"
 echo
 echo "This step is COMPLETELY OPTIONAL, feel free to select None and finish the installation process"
 echo
@@ -8,7 +8,7 @@ echo ">>> Desktop Environment <<<"
 echo
 while ! [[ "$desktop" =~ ^(1|2|3|4|5|6|7)$ ]] 
 do
-    echo "Please select 1,2,3,4,5,6,7 for:"
+    echo "Please select one option:"
     echo "1. GNOME Minimal"
     echo "2. GNOME Full (beware of pkgs count)"
     echo "3. KDE Plasma"
@@ -16,8 +16,8 @@ do
     echo "5. LXQt"	
     echo "6. LXDE"
     echo "7. Cinnamon"
-    echo "8. None - Quit"
-    read -p "Desktop: " desktop
+    echo "8. None - I don't want bloat"
+    read -p "Desktop (1-8): " desktop
 done
 case $desktop in
     1)
@@ -72,32 +72,58 @@ case $desktop in
         arch-chroot /mnt /bin/bash -c "systemctl enable lxdm.service"
     	;;
 esac
-# install KVM drivers (video qxl is disabled due to bugs on certain DEs)
+# install KVM drivers (xf86-video-qxl is disabled due to bugs on certain DEs)
 vm=$(arch-chroot /mnt /bin/bash -c "systemd-detect-virt")
 if [[ $vm = "kvm" ]]
 then
     arch-chroot /mnt /bin/bash -c "pacman -S spice-vdagent --noconfirm --needed"
-    #arch-chroot /mnt /bin/bash -c "pacman -S spice-vdagent xf86-video-qxl --noconfirm --needed"
 fi
 clear
+# nvidia
 echo ">>> NVIDIA Support <<<"
 echo
-echo "Do you want to add NVIDIA support? (Y/N)"
+echo "Do you want to add propietary NVIDIA drivers? (Y/N)"
 read -p "NVIDIA Support: " nvidia
 if [[ $nvidia == "y" || $nvidia == "Y" || $nvidia == "yes" || $nvidia == "Yes" ]]
 then
-    arch-chroot /mnt /bin/bash -c "pacman -S nvidia nvidia-utils egl-wayland --noconfirm --needed"
+    arch-chroot /mnt /bin/bash -c "pacman -S nvidia-dkms nvidia-utils egl-wayland --noconfirm --needed"
 fi
 clear
+# broadcom
+echo ">>> Broadcom WiFi Support <<<"
+echo
+echo "Only do this if your Broadcom card doesn't works with built-in kernel drivers"
+echo
+echo "Do you want to add propietary Broadcom drivers? (Y/N)"
+read -p "Broadcom Support: " broadcom
+if [[ $broadcom == "y" || $broadcom == "Y" || $broadcom == "yes" || $broadcom == "Yes" ]]
+then
+    arch-chroot /mnt /bin/bash -c "pacman -S broadcom-wl-dkms --noconfirm --needed"
+fi
+clear
+# intel vaapi
+echo ">>> Intel VAAPI drivers (recommended) <<<"
+echo
+echo "Only do this if you have an Intel GPU"
+echo
+echo "Do you want to add Intel VAAPI? (Y/N)"
+read -p "Intel VAAPI: " intelVaapi
+if [[ $intelVaapi == "y" || $intelVaapi == "Y" || $intelVaapi == "yes" || $intelVaapi == "Yes" ]]
+then
+    arch-chroot /mnt /bin/bash -c "pacman -S libva-intel-driver intel-media-driver vainfo --noconfirm --needed"
+fi
+clear
+# flatpak
 echo ">>> Flatpak <<<"
 echo
-echo "Do you want to install flatpak? (Y/N)"
+echo "Do you want to install Flatpak? (Y/N)"
 read -p "Flatpak: " flatpak
 if [[ $flatpak == "y" || $flatpak == "Y" || $flatpak == "yes" || $flatpak == "Yes" ]]
 then
     arch-chroot /mnt /bin/bash -c "pacman -S flatpak --noconfirm --needed"
 fi
 clear
+# cups
 echo ">>> Printer Support (CUPS) <<<"
 echo
 echo "Do you want to add printing support? (Y/N)"
