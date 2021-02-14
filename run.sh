cd80b73981a1d4ad65a72dbbf71adc8cabcc92f1
Victor Bayas <victorsbayas@gmail.com>
2021-02-13 19:05:09 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@
 clear
 echo "SimplyArch bootstrapper..."
 echo
-pacman -Sy glibc git --noconfirm --needed
+pacman -S git --noconfirm --needed
 git clone https://github.com/geminis3/simplyarch
 cd simplyarch
 chmod +x simplyarch.sh
