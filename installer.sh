0a827868537162dc7e91d76bd82ee710732e4d93
Victor Bayas <victorsbayas@gmail.com>
2021-09-29 22:27:20 -0500

    Merge SimplyArch 2.0 (#10)
    SimplyArch has been rewritten to become faster,
    easier to mantain and give more choices to the user.
    
    Installer Highlights:
    
    - More friendly user messages
    - Choose between the official Arch kernel flavors
    - User can choose between Yay, Paru or no AUR helper
    - rate-mirrors and multitheaded Pacman for faaast install speeds
    - Removed useless pkgs from base for a true SimplyArch experience
    
    Postinstaller Highlights:
    
    - Detect AUR helper installed on user's system
    - MATE and Cutefish offered as DE options
    - General pkg cleanup for all DE
    - rate-mirrors installed on user's system for convenience
    - Automatic guest additions installation for VBox, KVM and VMWare
    - Smart driver installer for Nvidia, Optimus, Broadcom and VAAPI
    - More apps available for installation
    
    Enjoy!

---
+++ installer.sh
@@ -0,0 +1,437 @@
+#!/bin/bash
+
+# This is the core script used by SimplyArch Installer
+# Copyright (C) The SimplyArch Authors
+
+# Released under the MIT license
+
+# Function declaration begins
+
+# Message displayed to user at start
+greeting(){
+    echo
+    echo "Welcome to SimplyArch Installer v2"
+    echo "Copyright (C) The SimplyArch Authors"
+    echo
+    echo "DISCLAIMER: THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED"
+    echo
+    echo "WARNING: MAKE SURE TO TYPE CORRECTLY BECAUSE THE SCRIPT WON'T PERFORM INPUT VALIDATIONS"
+    echo
+    echo "We'll guide you through the Arch Linux installation process"
+    echo
+}
+
+# Gather data about the user's computer
+analyze_host(){
+    echo
+    echo "System Analysis:"
+    echo
+    # Check BIOS type
+    if [[ -d /sys/firmware/efi ]]
+    then
+        bios_type="uefi"
+        echo "Detected UEFI"
+    else
+        bios_type="bios"
+        echo "Detected BIOS"
+    fi
+    # Check CPU vendor
+    if [ -n "$(lscpu | grep GenuineIntel)" ]
+    then
+        cpu_vendor="intel"
+        echo "Detected Intel CPU"
+    elif [ -n "$(lscpu | grep AuthenticAMD)" ]
+    then
+        cpu_vendor="amd"
+        echo "Detected AMD CPU"
+    fi
+    sleep 3
+}
+
+# Language & Keyboard setup
+locales(){
+    echo
+    echo "1. Language & Keyboard Setup"
+    echo
+    echo "HINT: write en_US for English US (don't add .UTF-8)"
+    echo
+    read -p "> System Language: " language
+    # If no input from user then default to en_US
+    if [[ -z "$language" ]]
+	then
+		language="en_US"
+	fi
+    echo
+    echo "EXAMPLES: us United States | us-acentos US Intl | latam Latin American Spanish | es Spanish"
+    echo
+    read -p "> Keyboard Distribution: " keyboard
+    # If no input from user then default to us keyboard
+    if [[ -z "$keyboard" ]]
+	then
+		keyboard="us"
+	fi
+    # Load selected keyboard distribution
+    loadkeys "$keyboard"
+}
+
+# Account setup
+user_accounts(){
+    echo
+    echo "2. User Accounts"
+    echo
+    read -p "> Choose a hostname for this computer: " hostname
+    # If no input from user then default to archlinux
+    if [[ -z "$hostname" ]]
+	then
+		hostname="archlinux"
+	fi
+	echo
+	echo "Administrator User"
+	echo "User: root"
+	read -sp "> Password: " root_password
+	echo
+	read -sp "> Re-type password: " root_password2
+	echo
+	while [[ "$root_password" != "$root_password2" ]]
+	do
+		echo
+		echo "Passwords don't match. Try again"
+		echo
+		read -sp "> Password: " root_password
+		echo
+		read -sp "> Re-type password: " root_password2
+		echo
+	done
+	echo
+	echo "Standard User"
+	read -p "> User: " user
+	export user
+	read -sp "> Password: " user_password
+	echo
+	read -sp "> Re-type password: " user_password2
+	echo
+	while [[ "$user_password" != "$user_password2" ]]
+	do
+		echo
+		echo "Passwords don't match. Try again"
+		echo
+		read -sp "> Password: " user_password
+		echo
+		read -sp "> Re-type password: " user_password2
+		echo
+	done
+}
+
+# Disk setup
+disks(){
+    echo
+    echo "3. Disks Setup"
+	echo
+	echo "Make sure to have your disk previously partitioned, if you are unsure re-run this script when done"
+    echo
+    echo "HINT: you can use cfdisk, fdisk, parted or the tool of your preference"
+    echo "For reference partition layouts check out the Arch Wiki entry at https://bit.ly/3m4I33p"
+	echo
+    echo "Your current partition table:"
+    echo
+    lsblk
+    echo
+    read -p "> Do you want to continue? (Y/N): " prompt
+    if [[ "$prompt" == "y" || "$prompt" == "Y" || "$prompt" == "yes" || "$prompt" == "Yes" ]]
+    then
+        # Filesystem
+        clear
+        echo
+        echo "3. Disks Setup"
+        while ! [[ "$filesystem" =~ ^(1|2)$ ]]
+        do
+            echo
+            echo "Choose a filesystem for your root partition:"
+            echo
+            echo "1. EXT4 (the preferred choice for most users)"
+            echo "2. BTRFS (a FS with built-in snapshot functionality)"
+            echo
+            read -p "> Filesystem (1-2): " filesystem
+        done
+        # Root partition
+        clear
+        echo
+        echo "3. Disks Setup"
+        echo
+        echo "Your current partition table:"
+        echo
+        lsblk
+        echo
+        echo "Write the name of the partition e.g: /dev/sdaX /dev/nvme0n1pX"
+        read -p "> Root partition: " root_partition
+        case "$filesystem" in
+            1)
+                mkfs.ext4 "$root_partition"
+                mount "$root_partition" /mnt
+                ;;
+            2)
+                mkfs.btrfs -f -L "Arch Linux" "$root_partition"
+                mount "$root_partition" /mnt
+                btrfs sub cr /mnt/@
+                umount "$root_partition"
+                mount -o relatime,space_cache=v2,compress=lzo,subvol=@ "$root_partition" /mnt
+                mkdir /mnt/boot
+                ;;
+        esac
+        # EFI only needed for UEFI
+        if [[ "$bios_type" == "uefi" ]]
+        then
+        clear
+        echo
+        echo "3. Disks Setup"
+        echo
+        echo "Your current partition table:"
+        echo
+        lsblk
+        echo
+        echo "Write the name of the partition e.g: /dev/sdaX /dev/nvme0n1pX"
+        read -p "> EFI partition: " efi_partition
+        echo
+        echo "HINT: If you're dualbooting another OS type N otherwise Y"
+        read -p "> Do you want to format this EFI partition as FAT32? (Y/N): " format_efi
+        if [[ "$format_efi" == "y" || "$format_efi" == "Y" || "$format_efi" == "yes" || "$format_efi" == "Yes" ]]
+        then
+            mkfs.fat -F32 "$efi_partition"
+        fi
+        mkdir -p /mnt/boot/efi
+        mount "$efi_partition" /mnt/boot/efi
+        fi
+        # Swap
+        clear
+        echo "3. Disks Setup"
+        echo
+        echo "Your current partition table:"
+        echo
+        lsblk
+        echo
+        echo "HINT: If you don't want to use a Swap partition type N below"
+        echo
+        echo "Write the name of the partition e.g: /dev/sdaX /dev/nvme0n1pX"
+        read -p "> Swap partition: " swap
+        if [[ "$swap" == "n" || "$swap" == "N" || "$swap" == "no" || "$swap" == "No" ]]
+        then
+            echo
+            echo "Swap partition not selected"
+            sleep 1
+        else
+            mkswap "$swap"
+            swapon "$swap"
+        fi
+    else
+        echo
+        echo "Installer aborted..."
+        exit
+    fi
+}
+
+# Allow user to select the kernel of their choice
+kernel_selector(){
+    while ! [[ "$kernel_flavor" =~ ^(1|2|3|4)$ ]]
+    do
+    echo
+    echo "4. Kernel Selector"
+    echo
+    echo "Choose a kernel for your system:"
+    echo
+    echo "1. linux (the preferred choice for most users)"
+    echo "2. linux-lts (a long-term supported kernel)"
+    echo "3. linux-zen (a performance focused kernel)"
+    echo "4. linux-hardened (a security focused kernel)"
+    echo
+    read -p "> Kernel flavor (1-4): " kernel_flavor
+    done
+    case "$kernel_flavor" in
+        1)
+            kernel_flavor="linux"
+            ;;
+        2)
+            kernel_flavor="linux-lts"
+            ;;
+        3)
+            kernel_flavor="linux-zen"
+            ;;
+        4)
+            kernel_flavor="linux-hardened"
+            ;;
+    esac
+}
+
+# Install an AUR helper
+aur_installer(){
+    while ! [[ "$aur_helper" =~ ^(1|2|3)$ ]]
+    do
+    echo
+    echo "5. AUR Installer"
+    echo
+    echo "Choose an AUR helper for your system:"
+    echo
+    echo "1. Yay"
+    echo "2. Paru"
+    echo "3. No AUR helper"
+    echo
+    read -p "> AUR helper (1-3): " aur_helper
+    done
+    case "$aur_helper" in
+        1)
+            clear
+            echo
+            echo "5. AUR Installer"
+            echo
+            echo "Installing Yay..."
+            echo
+            arch-chroot /mnt /bin/bash -c "pacman -Sy git --noconfirm --needed"
+            echo "cd && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si --noconfirm && cd && rm -rf yay-bin" | arch-chroot /mnt /bin/bash -c "su $user"
+            ;;
+        2)
+            clear
+            echo
+            echo "5. AUR Installer"
+            echo
+            echo "Installing Paru..."
+            echo
+            arch-chroot /mnt /bin/bash -c "pacman -Sy git --noconfirm --needed"
+            echo "cd && git clone https://aur.archlinux.org/paru-bin.git && cd paru-bin && makepkg -si --noconfirm && cd && rm -rf paru-bin" | arch-chroot /mnt /bin/bash -c "su $user"
+            ;;
+    esac
+}
+
+# Performs the actual system install
+arch_installer(){
+    # Install the base packages
+    case "$bios_type" in
+        "bios" )
+            pacstrap /mnt base base-devel "$kernel_flavor" "$kernel_flavor"-headers linux-firmware grub os-prober sudo bash-completion networkmanager nano reflector xdg-user-dirs
+            ;;
+        "uefi" )
+            pacstrap /mnt base base-devel "$kernel_flavor" "$kernel_flavor"-headers linux-firmware grub efibootmgr os-prober sudo bash-completion networkmanager nano reflector xdg-user-dirs
+            ;;
+    esac
+    # Generate fstab with UUID
+    genfstab -U /mnt >> /mnt/etc/fstab
+    # Set language
+    echo "$language.UTF-8 UTF-8" > /mnt/etc/locale.gen
+	arch-chroot /mnt /bin/bash -c "locale-gen"
+	echo "LANG=$language.UTF-8" > /mnt/etc/locale.conf
+    # Set keyboard
+    echo "KEYMAP=$keyboard" > /mnt/etc/vconsole.conf
+    # Auto-detect timezone
+    arch-chroot /mnt /bin/bash -c "ln -sf /usr/share/zoneinfo/$(curl https://ipapi.co/timezone) /etc/localtime"
+	arch-chroot /mnt /bin/bash -c "hwclock --systohc"
+    # Pacman tweaks
+    # Enable multilib
+	sed -i '93d' /mnt/etc/pacman.conf
+	sed -i '94d' /mnt/etc/pacman.conf
+	sed -i "93i [multilib]" /mnt/etc/pacman.conf
+	sed -i "94i Include = /etc/pacman.d/mirrorlist" /mnt/etc/pacman.conf
+    # Enable parallel downloads for faaaast speeds
+    sed -i "s/#ParallelDownloads/ParallelDownloads/" /mnt/etc/pacman.conf
+    # Enable easter egg
+    sed -i 's/#Color/Color/' /mnt/etc/pacman.conf
+    sed -i '34i ILoveCandy' /mnt/etc/pacman.conf
+    # Set hostname
+    echo "$hostname" > /mnt/etc/hostname
+	echo "127.0.0.1	localhost" > /mnt/etc/hosts
+	echo "::1		localhost" >> /mnt/etc/hosts
+	echo "127.0.1.1	$hostname.localdomain	$hostname" >> /mnt/etc/hosts
+    # Install appropiate CPU microcode
+    case "$cpu_vendor" in
+        "intel" )
+            arch-chroot /mnt /bin/bash -c "pacman -Sy intel-ucode --noconfirm --needed"
+            ;;
+        "amd" )
+            arch-chroot /mnt /bin/bash -c "pacman -Sy amd-ucode --noconfirm --needed"
+            ;;
+    esac
+    # Install bootloader
+    case "$bios_type" in
+    "bios" )
+        arch-chroot /mnt /bin/bash -c "grub-install --target=i386-pc ${root_partition::-1}"
+        ;;
+    "uefi" )
+        arch-chroot /mnt /bin/bash -c "grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=Arch"
+        ;;
+    esac
+    arch-chroot /mnt /bin/bash -c "grub-mkconfig -o /boot/grub/grub.cfg"
+    # Enable Network Manager
+	arch-chroot /mnt /bin/bash -c "systemctl enable NetworkManager.service"
+	# Set root user password
+	arch-chroot /mnt /bin/bash -c "(echo $root_password ; echo $root_password) | passwd root"
+	# Setup user
+	arch-chroot /mnt /bin/bash -c "useradd -m -G wheel $user"
+	arch-chroot /mnt /bin/bash -c "(echo $user_password ; echo $user_password) | passwd $user"
+	arch-chroot /mnt sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers
+	arch-chroot /mnt /bin/bash -c "xdg-user-dirs-update"
+    # Update mirrors for installed system
+    clear
+    echo
+    echo "Updating mirrors for installed system, please wait..."
+    echo
+    sudo -u nobody ./bin/rate_mirrors arch | tee /mnt/etc/pacman.d/mirrorlist
+    # AUR installer
+    clear
+    aur_installer
+}
+
+# Byeeeeee
+goodbye(){
+    echo
+    echo "Thank you for using SimplyArch Installer!"
+    echo
+    echo "Installation finished successfully"
+    echo
+    read -p "> Would you like to reboot your computer? (Y/N): " prompt
+    if [[ "$prompt" == "y" || "$prompt" == "Y" || "$prompt" == "yes" || "$prompt" == "Yes" ]]
+    then
+        echo
+        echo "System will reboot in a moment..."
+		sleep 3
+		clear
+		umount -a
+		reboot
+    else
+        exit
+    fi
+}
+
+# Function declaration ends
+
+# Execution order
+
+clear
+greeting
+read -p "> Do you want to continue? (Y/N): " prompt
+if [[ "$prompt" == "y" || "$prompt" == "Y" || "$prompt" == "yes" || "$prompt" == "Yes" ]]
+then
+    clear
+    analyze_host
+    clear
+    locales
+    clear
+    user_accounts
+    clear
+    disks
+    clear
+    kernel_selector
+    clear
+    # Update mirrors before install
+    echo
+    echo "Updating mirrors for faster install, please wait..."
+    echo
+    sudo -u nobody ./bin/rate_mirrors arch | tee /etc/pacman.d/mirrorlist
+    clear
+    arch_installer
+    clear
+    chmod +x ./postinstall.sh
+    ./postinstall.sh
+    clear
+    goodbye
+else
+    echo
+    echo "Installer aborted..."
+    exit
+fi
--- run.sh
+++ run.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-# short URL: https://is.gd/simplyarch
-# see stats at: https://is.gd/stats.php?url=simplyarch
+# This script is used to bootstrap SimplyArch Installer
+# Copyright (C) The SimplyArch Authors
+
+# Released under the MIT license
+
+# Usage:
+# curl -L is.gd/simplyarch > run ; sh run
+
+# See our stats at: https://is.gd/stats.php?url=simplyarch
 
 clear
-echo "SimplyArch bootstrapper..."
+echo "SimplyArch Installer will start in a few seconds..."
 echo
 pacman -Sy glibc --noconfirm
 pacman -S git --noconfirm
 git clone https://github.com/geminis3/simplyarch
-cd simplyarch
-chmod +x simplyarch.sh
-./simplyarch.sh
+cd simplyarch || exit
+chmod +x installer.sh
+./installer.sh
